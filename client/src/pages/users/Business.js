d42b185531f35f0c461f589488eb4a15cc2bd02d
harishmuleva <harish_muleva@dell.com>
2023-07-05 20:59:56 +0530

    Client side user, business, education and address completed

--- client/src/pages/users/Business.js
+++ client/src/pages/users/Business.js
@@ -1,104 +1,337 @@
-import React from "react";
-import {
-  Card,
-  Avatar,
-  Divider,
-  Typography,
-  Form,
-  Input,
-  Button,
-  Select,
-  message,
-  Modal,
-} from "antd";
+import React, { useState } from "react";
+import { Table, Input, Select, Button } from "antd";
+import { useCreate } from "@refinedev/core";
 
-import { UserOutlined } from "@ant-design/icons";
-import { Create, CreateButton, useForm } from "@refinedev/antd";
-import axios from "axios";
-const API_URL = process.env.REACT_APP_API_SERVER
-const TOKEN_KEY = process.env.REACT_APP_TOKEN_KEY
+const BusinessDetails = ({userid}) => {
+  const [data, setData] = useState([]);
+  const [editingKey, setEditingKey] = useState("");
+  const { mutate } = useCreate();
 
-const { Meta } = Card;
-const { Title } = Typography;
-const { Option } = Select;
+  const [newBusiness, setNewBusiness] = useState({
+    ownerName: "",
+    businessName: "",
+    category: "",
+    businessType: "",
+    businessRole: "",
+    turnover: "",
+    size: "",
+    businessSector: ""
+  });
 
-const BusinessProfile = ({
-  parentId,
-  isBusinessModalOpen,
-  setIsBusinessModalOpen,
-}) => {
-  console.log("parentId", parentId);
-  const { formProps, getInputProps, saveButtonProps } = useForm();
+  const categories = ["Category 1", "Category 2", "Category 3"];
+  const businessTypes = ["Type 1", "Type 2", "Type 3"];
+  const businessRoles = ["Role 1", "Role 2", "Role 3"];
+  const businessSectors = ["Sector 1", "Sector 2", "Sector 3"];
 
-  const showModal = () => {
-    setIsBusinessModalOpen(true);
+  const columns = [
+    {
+      title: "Owner Name",
+      dataIndex: "ownerName",
+      key: "ownerName",
+      editable: true,
+      render: (_, record) => (
+        <Input
+          value={record.ownerName}
+          onChange={(e) => handleInputChange(e, record.key, "ownerName")}
+        />
+      ),
+    },
+    {
+      title: "Business Name",
+      dataIndex: "businessName",
+      key: "businessName",
+      editable: true,
+      render: (_, record) => (
+        <Input
+          value={record.businessName}
+          onChange={(e) => handleInputChange(e, record.key, "businessName")}
+        />
+      ),
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+      editable: true,
+      render: (_, record) => (
+        <Select
+          value={record.category}
+          onChange={(value) => handleSelectChange(value, record.key, "category")}
+        >
+          {categories.map((category) => (
+            <Select.Option key={category} value={category}>
+              {category}
+            </Select.Option>
+          ))}
+        </Select>
+      ),
+    },
+    {
+      title: "Business Type",
+      dataIndex: "businessType",
+      key: "businessType",
+      editable: true,
+      render: (_, record) => (
+        <Select
+          value={record.businessType}
+          onChange={(value) => handleSelectChange(value, record.key, "businessType")}
+        >
+          {businessTypes.map((type) => (
+            <Select.Option key={type} value={type}>
+              {type}
+            </Select.Option>
+          ))}
+        </Select>
+      ),
+    },
+    {
+      title: "Business Role",
+      dataIndex: "businessRole",
+      key: "businessRole",
+      editable: true,
+      render: (_, record) => (
+        <Select
+          value={record.businessRole}
+          onChange={(value) => handleSelectChange(value, record.key, "businessRole")}
+        >
+          {businessRoles.map((role) => (
+            <Select.Option key={role} value={role}>
+              {role}
+            </Select.Option>
+          ))}
+        </Select>
+      ),
+    },
+    {
+      title: "Turnover",
+      dataIndex: "turnover",
+      key: "turnover",
+      editable: true,
+      render: (_, record) => (
+        <Input
+          value={record.turnover}
+          onChange={(e) => handleInputChange(e, record.key, "turnover")}
+        />
+      ),
+    },
+    {
+      title: "Size",
+      dataIndex: "size",
+      key: "size",
+      editable: true,
+      render: (_, record) => (
+        <Input
+          value={record.size}
+          onChange={(e) => handleInputChange(e, record.key, "size")}
+        />
+      ),
+    },
+    {
+      title: "Business Sector",
+      dataIndex: "businessSector",
+      key: "businessSector",
+      editable: true,
+      render: (_, record) => (
+        <Select
+          value={record.businessSector}
+          onChange={(value) => handleSelectChange(value, record.key, "businessSector")}
+        >
+          {businessSectors.map((sector) => (
+            <Select.Option key={sector} value={sector}>
+              {sector}
+            </Select.Option>
+          ))}
+        </Select>
+      ),
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (_, record) => (
+        <span>
+          {editingKey === record.key ? (
+            <>
+              <Button type="primary" onClick={() => save(record.key)}>
+                Save
+              </Button>
+              <Button onClick={cancel}>Cancel</Button>
+            </>
+          ) : (
+            <>
+              
+             
+              <Button onClick={() => handleRemove(record.key)}>Remove</Button>
+
+            </>
+          )}
+        </span>
+      ),
+    },
+  ];
+
+  const handleInputChange = (e, key, field) => {
+    const { value } = e.target;
+    const updatedData = [...data];
+    const target = updatedData.find((item) => item.key === key);
+    if (target) {
+      target[field] = value;
+      setData(updatedData);
+    }
   };
 
-  const handleOk = () => {
-    console.log("handle Ok clicked ");
-    setIsBusinessModalOpen(false);
+  const handleSelectChange = (value, key, field) => {
+    const updatedData = [...data];
+    const target = updatedData.find((item) => item.key === key);
+    if (target) {
+      target[field] = value;
+      setData(updatedData);
+    }
+  };
+
+  const handleAdd = () => {
+    const newData = {
+      key: `${data.length + 1}`,
+      ownerName: newBusiness.ownerName,
+      businessName: newBusiness.businessName,
+      category: newBusiness.category,
+      businessType: newBusiness.businessType,
+      businessRole: newBusiness.businessRole,
+      turnover: newBusiness.turnover,
+      size: newBusiness.size,
+      businessSector: newBusiness.businessSector,
+    };
+    setData([...data, newData]);
+    setNewBusiness({
+      ownerName: "",
+      businessName: "",
+      category: "",
+      businessType: "",
+      businessRole: "",
+      turnover: "",
+      size: "",
+      businessSector: ""
+    });
+  };
+
+  const handleRemove = (key) => {
+    const updatedData = data.filter((item) => item.key !== key);
+    setData(updatedData);
+  };
+
+
+  const handleSave = (key) => {
+    setEditingKey("");
   };
 
   const handleCancel = () => {
-    setIsBusinessModalOpen(false);
+    setEditingKey("");
   };
 
-  const handleFormFinish = async(values) => {
-    values = { ...values, userid: parentId };
-    console.log("Submitted form values:", values);
-    try {
-      const response = await axios.post(API_URL + '/api/businesses', {
-          data:values,
-      });
+  const isEditing = (record) => record.key === editingKey;
 
-      console.log('Response:', response.data);
-      setIsBusinessModalOpen(false);
-      // Perform any necessary actions after successful data submission
-     
-  }
-
-  catch (error) {
-      console.error('Error:', error);
-      // Handle any errors that occur during the data submission
-  }
-    setIsBusinessModalOpen(false);
+  const components = {
+    body: {
+      cell: EditableCell,
+    },
   };
+
+  const columnsWithEditability = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
 
   return (
-    <>
-      <Modal
-        title="Basic Modal"
-        visible={isBusinessModalOpen}
-       
-        width={1220}
-        destroyOnClose
-        footer={null}
-      >
-        <Form
-          {...formProps}
-          layout="vertical"
-          onFinish={(values) => formProps.onFinish?.(handleFormFinish(values))}
-        >
-          <Form.Item
-            name="name"
-            label="BusinessName"
-            rules={[
-              { required: true, message: "Please enter your Businessname" },
-            ]}
-          >
-            <Input placeholder="Business" />
-          </Form.Item>
-          {/* Rest of the form fields */}
-          <Divider />
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </>
+    <div>
+      <div>
+        <Button onClick={handleAdd} type="primary" style={{ marginBottom: 16 }}>
+          + Add Business
+        </Button>
+        <Button type="default" onClick={()=>{
+      console.log("save data",data)
+          data.map((item)=>{
+            console.log("item",item)
+            const {key, ...remain} = item;
+            // const [[key, value], ...remain] = item;
+            // console.log("key", key);
+            remain["turnover"]=parseFloat(item.turnover)
+            remain["size"]=parseInt(item.size)
+            console.log("remain", remain);
+           remain['userid']= userid
+            mutate({
+              resource: "businesses",
+              values: remain,
+          });
+          })
+
+    }}>Save Businesses</Button>
+      </div>
+      <Table
+        components={components}
+        bordered
+        dataSource={data}
+        columns={columnsWithEditability}
+        rowClassName={() => "editable-row"}
+        pagination={false}
+      />
+    </div>
   );
 };
 
-export default BusinessProfile;
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === "select" ? (
+    <Select style={{ width: "100%" }}>
+      <Select.Option value="option1">Option 1</Select.Option>
+      <Select.Option value="option2">Option 2</Select.Option>
+      <Select.Option value="option3">Option 3</Select.Option>
+    </Select>
+  ) : (
+    <Input />
+  );
+  return (
+    
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    
+    </td>
+  );
+};
+
+export default BusinessDetails;
--- client/src/pages/users/create.js
+++ client/src/pages/users/create.js
@@ -1,14 +1,13 @@
-import React from 'react';
 import { Create, useForm } from '@refinedev/antd';
 
-import { Col, Row, Form, Input, Select, Upload, Button, Space,DatePicker } from 'antd';
-import { MinusCircleOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons';
 import { getValueProps, mediaUploadMapper } from "@refinedev/strapi-v4";
-import Personal from './Personal';
+import { Col, DatePicker, Form, Input, Row, Select, Upload } from 'antd';
 import Business from './Business';
+import Personal from './Personal';
 
-import { Tabs } from 'antd';
 import Education from './Education';
+const API_URL = process.env.REACT_APP_API_SERVER
+const TOKEN_KEY = process.env.REACT_APP_TOKEN_KEY
 const onChange = (key) => {
   console.log(key);
 };
@@ -16,139 +15,180 @@ const items = [
   {
     key: '1',
     label: `Personal Profile`,
-    children: <Personal/>,
+    children: <Personal />,
   },
   {
     key: '2',
     label: `Business Profile`,
-    children: <Business/>,
+    children: <Business />,
   },
   {
     key: '3',
     label: `Education Profile`,
-    children: <Education/>,
+    children: <Education />,
   },
 ];
+const { TextArea } = Input;
+
 
 const UserCreate = () => {
-    const { formProps, getInputProps, saveButtonProps } = useForm();
-    const handleFormFinish = (values) => {
-        values={...values, role:2}
-        console.log("values", values)
-        // values={...values, classes:selectedClassId,subject:selectedSubjectId,topics:selectedTopicId}
-        
-        formProps.onFinish?.(mediaUploadMapper(values));
-        // Handle form submission with mappedValues
+  const { formProps, getInputProps, saveButtonProps } = useForm();
+  const handleFormFinish = (values) => {
+    values = { ...values, role: 2 }
+    console.log("values", values)
+    // values={...values, classes:selectedClassId,subject:selectedSubjectId,topics:selectedTopicId}
 
-    };
-    return (
-        <div>
-         <Create saveButtonProps={saveButtonProps}>
+    formProps.onFinish?.(mediaUploadMapper(values));
+    // Handle form submission with mappedValues
 
-            <Form {...formProps} layout="vertical" onFinish={handleFormFinish}>
-            <Row>
-          <Col span={8}>
-    <Form.Item label="Username Name" name="username" 
-    rules={[{ required: true, message: 'Please enter your Username' }]}>
-      <Input placeholder="Username" />
-    </Form.Item>
-    </Col>
-    <Col span={8}>
-    <Form.Item label="email" name="email" 
-    rules={[{ required: true, message: 'Please enter your email' }]}>
-      <Input placeholder="Email" />
-    </Form.Item>
-</Col>
-    <Col span={8}>
-    <Form.Item label="password" name="password" 
-    rules={[{ required: true, message: 'Please enter your password' }]}>
-      <Input placeholder=" Password" />
-    </Form.Item>
-    </Col>
-    </Row>
+  };
+  const PersonalProfile = () => {
+  }
+  return (
+    <div>
+      <Create saveButtonProps={saveButtonProps}>
+        <Form {...formProps} layout="vertical" onFinish={handleFormFinish}>
+          <Row>
+            <Col span={24}>  
+            <Form.Item
+              name="photo"
+              valuePropName="photo"
+              getValueProps={(data) => getValueProps(data, API_URL)}
+              noStyle
+            >
+              <Upload.Dragger
+                name="files"
+                action={`${API_URL}/api/upload`}
+                headers={{
+                 
+                  Authorization: `Bearer ${localStorage.getItem(
+                    TOKEN_KEY,
+                   
+                  )}`,
+                }}
+                accept="image/*"
+              >
+                <p className="ant-upload-text">
+                   Photo
+                </p>
+              </Upload.Dragger>
+            </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={8}>
+              <Form.Item label="Username Name" name="username"
+                rules={[{ required: true, message: 'Please enter your Username' }]}>
+                <Input placeholder="Username" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="email" name="email"
+                rules={[{ required: true, message: 'Please enter your email' }]}>
+                <Input placeholder="Email" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="password" name="password"
+                rules={[{ required: true, message: 'Please enter your password' }]}>
+                <Input placeholder=" Password" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={8}>
+              <Form.Item label="First Name" name="firstname"
+                rules={[{ required: true, message: 'Please enter your first name' }]}>
+                <Input placeholder="First Name" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="Last Name" name="lastname">
+                <Input placeholder="Last Name" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="Father Name" name="fathername">
+                <Input placeholder=" Father Name" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
 
-   
-    <Row>
-          <Col span={8}>
-    <Form.Item label="First Name" name="firstName" 
-    rules={[{ required: true, message: 'Please enter your first name' }]}>
-      <Input placeholder="First Name" />
-    </Form.Item>
-    </Col>
-    <Col span={8}>
-    <Form.Item label="Last Name" name="lastName" 
-    rules={[{ required: true, message: 'Please enter your last name' }]}>
-      <Input placeholder="Last Name" />
-    </Form.Item>
-</Col>
-    <Col span={8}>
-    <Form.Item label="Father Name" name="fatherName" 
-    rules={[{ required: true, message: 'Please enter your Father name' }]}>
-      <Input placeholder=" Father Name" />
-    </Form.Item>
-    </Col>
-    </Row>
+            <Col span={8}>
+              <Form.Item label="Mother Name" name="mothername">
+                <Input placeholder=" Mother Name" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="Gender" name="gender" rules={[{ required: true, message: 'Please select your gender' }]}>
+                <Select placeholder="Select Gender">
+                  <Option value="male">Male</Option>
+                  <Option value="female">Female</Option>
+                  <Option value="other">Other</Option>
+                </Select>
+              </Form.Item>
+            </Col>
 
-   
-
-    <Row>
-
-    <Col span={8}>
-    <Form.Item label="Mother Name" name="motherName" 
-    rules={[{ required: true, message: 'Please enter your Mother name' }]}>
-      <Input placeholder=" Mother Name" />
-    </Form.Item>
-    </Col>
-    <Col span={8}>
-    <Form.Item label="Gender" name="gender" rules={[{ required: true, message: 'Please select your gender' }]}>
-      <Select placeholder="Select Gender">
-        <Option value="male">Male</Option>
-        <Option value="female">Female</Option>
-        <Option value="other">Other</Option>
-      </Select>
-    </Form.Item>
-    </Col>
-  
-    <Col span={8}>
-    <Form.Item label="Date of Birth" name="dob" rules={[{ required: true, message: 'Please select your date of birth' }]}>
-      <DatePicker style={{ width: '100%' }} placeholder="Select Date" />
-    </Form.Item>
-    </Col>
-    </Row>
+            <Col span={8}>
+              <Form.Item label="Date of Birth" name="dob" >
+                <DatePicker style={{ width: '100%' }} placeholder="Select Date" />
+              </Form.Item>
+            </Col>
+          </Row>
 
 
-    <Row>
+          <Row>
 
-    <Col span={8}>
-    <Form.Item label="Caste" name="caste" rules={[{ required: true, message: 'Please select your gender' }]}>
-      <Select placeholder="Select Caste">
-        <Option value="general">General</Option>
-        <Option value="obc">OBC</Option>
-        <Option value="sc/st">SC/ST</Option>
-      </Select>
-    </Form.Item>
-    </Col>
+            <Col span={8}>
+              <Form.Item label="Caste" name="cast" >
+                <Select placeholder="Select Cast">
+                  <Option value="general">General</Option>
+                  <Option value="obc">OBC</Option>
+                  <Option value="sc/st">SC/ST</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="Jati" name="jati" >
+                <Select placeholder="Select Jati">
+                  <Option value="SEERVI">SEERVI</Option>
+                  <Option value="JAAT">JAAT</Option>
+                  <Option value="SEN">SEN</Option>
+                </Select>
+              </Form.Item>
+            </Col>
 
-    
-   
-    <Col span={8}>
-    <Form.Item label="Martial status" name="martial" 
-    rules={[{ required: true, message: 'Please select your Martial staus' }]}
-    >
-      <Select placeholder="martial status">
-        <Option value="married">Married</Option>
-        <Option value="unmarried">Unmarried</Option>
-        <Option value="divorced">Divorced</Option>
-        
-      </Select>
-    </Form.Item>
-    </Col>
-    </Row>
-                
-            </Form>
-            </Create>
-        </div>
-    );
+
+            <Col span={8}>
+              <Form.Item label="Martial status" name="martial"
+                rules={[{ required: true, message: 'Please select your Martial staus' }]}
+              >
+                <Select placeholder="martial status">
+                  <Option value="married">Married</Option>
+                  <Option value="unmarried">Unmarried</Option>
+                  <Option value="divorced">Divorced</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            {/* <Col span={8}>
+              <Form.Item label="ISDivyang" name="isdivyang">
+                <Select placeholder="Select divyang">
+                  <Option value="YES">YES</Option>
+                  <Option value="NO">NO</Option>    
+                </Select>
+              </Form.Item>
+            </Col> */}
+            <Col span={8}>
+              <Form.Item label="DivyangDescription" name="divyangdescription">
+               <TextArea rows={4} placeholder="Divyang Description" maxLength={6} />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Create>
+    </div>
+  );
 };
 
 export default UserCreate;
