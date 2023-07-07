e34f6d0835cadc028f4c1c97cf81158986c38895
unknown <abhishekpawar0101@gmail.com>
2023-07-07 22:36:59 +0530

    Personal deatails and show card ready

---
+++ client/src/pages/users/PersonalDetails.js
@@ -0,0 +1,191 @@
+import { Badge, Dropdown, Space, Table } from 'antd'
+import { DownOutlined } from '@ant-design/icons';
+import { useShow } from "@refinedev/core";
+
+import React from 'react'
+
+export default function PersonalDetails(props) {
+    const record = props.record.data;
+    console.log("record", (record))
+
+    // const items = [
+    //     {
+    //         key: '1',
+    //         label: 'Action 1',
+    //     },
+    //     {
+    //         key: '2',
+    //         label: 'Action 2',
+    //     }
+    // ]
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'User Name',
+            dataIndex: 'username',
+            key: 'username',
+        },
+        {
+            title: 'EmailId',
+            dataIndex: 'email',
+            key: 'email',
+        },
+        {
+            title: 'Father Name',
+            key: 'father',
+            dataIndex: 'father'
+            // render: () => <Badge status="success" text="Finished" />,
+        },
+        {
+            title: 'Mobile number',
+            dataIndex: 'mobile',
+            key: 'mobile',
+        },
+        {
+            title: 'Jatti',
+            dataIndex: 'jatti',
+            key: 'jatti',
+        },
+        // {
+        //     title: 'Action',
+        //     dataIndex: 'operation',
+        //     key: 'operation',
+        //     render: () => (
+        //         <Space size="middle">
+        //             <a>Pause</a>
+        //             <a>Stop</a>
+        //             <Dropdown
+        //                 menu={{
+        //                     items,
+        //                 }}
+        //             >
+        //                 <a>
+        //                     More <DownOutlined />
+        //                 </a>
+        //             </Dropdown>
+        //         </Space>
+        //     ),
+        // },
+    ];
+
+    const data = [];
+
+
+    for (let i = 0; i < Array(record).length; ++i) {
+        data.push({
+            id: record.id,
+            username: record.username,
+            email: record.email,
+            father: record.father,
+            mobile: record.mobile,
+            jatti: record.jati,
+        });
+    }
+
+
+    
+        // record && Array.isArray(record) && record.map((ele, i) => {
+        //    return console.log("element", {i})
+        //     // return (
+        //     //     <>
+        //     //         data.push(
+        //     //         key:{ele.id.toString()},
+        //     //         date: '2014-12-24 23:12:00',
+        //     //         name: 'This is production name',
+        //     //         upgradeNum: 'Upgraded: 56',
+        //     //         );
+
+        //     //     </>
+        //     // );
+        // })
+    
+
+
+    const expandedRowRender = () => {
+        const columns = [
+            {
+                title: 'Id',
+                dataIndex: 'id',
+                key: 'id',
+            },
+            {
+                title: 'EmailId',
+                dataIndex: 'email',
+                key: 'email',
+            },
+            {
+                title: 'Father Name',
+                key: 'father',
+                render: () => <Badge status="success" text="Finished" />,
+            },
+            {
+                title: 'Mobile number',
+                dataIndex: 'mobile',
+                key: 'mobile',
+            },
+            {
+                title: 'Jatti',
+                dataIndex: 'jatti',
+                key: 'jatti',
+            },
+            // {
+            //     title: 'Date',
+            //     dataIndex: 'date',
+            //     key: 'date',
+            // },
+            // {
+            //     title: 'Name',
+            //     dataIndex: 'name',
+            //     key: 'name',
+            // },
+            // {
+            //     title: 'Status',
+            //     key: 'state',
+            //     render: () => <Badge status="success" text="Finished" />,
+            // },
+            // {
+            //     title: 'Upgrade Status',
+            //     dataIndex: 'upgradeNum',
+            //     key: 'upgradeNum',
+            // },
+
+        ];
+        // const data = [];
+        // for (let i = 0; i < 3; ++i) {
+        //     data.push({
+        //         key: i.toString(),
+        //         date: '2014-12-24 23:12:00',
+        //         name: 'This is production name',
+        //         upgradeNum: 'Upgraded: 56',
+        //     });
+        // }
+    }
+    return (
+        <>
+            <div>PersonalDetails</div>
+            <Table
+                columns={columns}
+                expandable={{
+                    expandedRowRender,
+                    defaultExpandedRowKeys: ['0'],
+                }}
+                dataSource={data}
+            />
+
+            {/* <Table
+                columns={columns}
+                expandable={{
+                    expandedRowRender,
+                    defaultExpandedRowKeys: ['0'],
+                }}
+                dataSource={data}
+                size="small"
+            /> */}
+        </>
+
+    )
+}
--- client/src/pages/users/show.js
+++ client/src/pages/users/show.js
@@ -6,63 +6,122 @@ import BusinessDetails from './Business';
 import EducationForm from './Education';
 import AddressDetails from './Address';
 import Family from './Family';
+import { image, reducer } from '@uiw/react-md-editor';
 const { Title, Text } = Typography;
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+import Personal from './Personal';
+import PersonalDetails from './PersonalDetails';
+const { Meta } = Card;
 
 const UserShow = () => {
-    const { queryResult } = useShow({metaData: { populate: ["businesses","educations", "addresses"]}});
-    const { data, isLoading } = queryResult;
-    console.log("User Show Data", data);
-    let businesslist=[]
-    let educationlist=[]
-    if(isLoading){
-      return <h1>Still loading </h1>
-    }
-    const record = data?.data;
-    businesslist=record?.businesses??[]
-    educationlist=record?.educationlist??[]
-    const items = [
-   
-        {
-          key: '1',
-          label: `Business Profile`,
-          children: <BusinessDetails userid={record?.id} businesslist={businesslist}/>,
-        },
-        {
-          key: '2',
-          label: `Education Profile`,
-          children: <EducationForm userid={record?.id} educationlist={educationlist}/>,
-        },
 
-        {
-          key: '3',
-          label: `Family`,
-          children: <Family userid={record?.id} />,
-        },
-        {
-            key: '4',
-            label: `Addresses`,
-            children: <AddressDetails userid={record?.id} />,
-          },
-      ];
-      
-      const onChange = (key) => {
-        
-        console.log(key);
-      };
-     
+  const { queryResult } = useShow({ metaData: { populate: ["businesses", "educations", "photo", "addresses"] } });
+  const { data, isLoading } = queryResult;
+  // console.log("User Show Data", data);
+  let businesslist = []
+  let educationlist = []
+  if (isLoading) {
+    return <h1>Still loading </h1>
+  }
+  const record = data?.data;
+  businesslist = record?.businesses ?? []
+  educationlist = record?.educationlist ?? []
+  // console.log("record", record)
 
-    return (
-        <Show isLoading={isLoading}>
 
-        <Title level={5}>Id</Title>
-        <Text>{record?.id}</Text>
+  let imgurl = record?.photo != null ? `${record?.photo?.formats?.thumbnail?.url}` : "https://www.gauchercommunity.org/wp-content/uploads/2020/09/avatar-placeholder-150x150.png"
+  // console.log("imageeurl",imgurl)
+  const items = [
 
-        <Title level={5}>Name</Title>
-        <Text>{record?.username}</Text>
-        {/* <Collapse items={items} defaultActiveKey={['1']} onChange={onChange} /> */}
-        <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+    {
+      key: '1',
+      label: `Personal Details`,
+      children: <PersonalDetails userid={record?.id} record={data} />,
+    },
+
+    {
+      key: '2',
+      label: `Business Profile`,
+
+      children: <BusinessDetails userid={record?.id} businesslist={businesslist} />,
+    },
+    {
+      key: '3',
+      label: `Education Profile`,
+      children: <EducationForm userid={record?.id} educationlist={educationlist} />,
+    },
+
+    {
+      key: '4',
+      label: `Family`,
+      children: <Family userid={record?.id} />,
+    },
+    {
+      key: '5',
+      label: `Addresses`,
+      children: <AddressDetails userid={record?.id} />,
+    },
+  ];
+
+  const onChange = (key) => {
+
+    // console.log(key);
+  };
+
+
+  return (<>
+
+
+
+    <Show isLoading={isLoading}>
+
+
+      <Card
+        style={{
+          width: 400,
+          margin: "auto",
+
+        }}
+      >
+        <div style={{
+          display: "flex",
+          flexDirection: "row"
+        }}>
+
+          <img style={{
+            borderRadius: '100%',
+            display: 'inline',
+          }}
+            alt="example"
+            src={imgurl}
+          />
+
+          <div style={{
+
+            // display: 'flex',
+            alignItems: 'center',
+            alignContent: 'center',
+            marginLeft: '60px'
+
+          }}>
+
+            <p>Name : <b>{record.firstname}  {record.lastname}</b></p>
+            {/* {console.log(record)} */}
+            <p>Caste: {record.cast}</p>
+            <p>DOB : {record.dob} </p>
+            <p>Marital : {record.marital} </p>
+          </div>
+        </div>
+
+      </Card>
+
+
+      {/* <Collapse items={items} defaultActiveKey={['1']} onChange={onChange} /> */}
+      <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     </Show>
-    );
+  </>
+  );
 };
 
 export default UserShow;
