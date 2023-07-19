b820c942494f7c720d26057211ebbcfca3a40f2d
harishmuleva <harish_muleva@dell.com>
2023-07-19 15:10:48 +0530

    pushing server updated

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,8 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name:'strapi::cors',
+    config:{
+      enabled: true,
+      origin: '*',
+      headers: '*'
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
