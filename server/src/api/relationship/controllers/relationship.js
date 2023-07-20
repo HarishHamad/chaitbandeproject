a667ec371d05024ac92f26ee355692fd2fb668ab
hcmuleva <hcmuleva@gmail.com>
2023-07-20 17:34:31 +0530

    Server for emeelan is working in linux system

--- server/src/api/relationship/controllers/relationship.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * relationship controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::relationship.relationship');
